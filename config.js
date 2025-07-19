981e20bca4da145af8c9d55c49f2d830b540da3d
REYSHOTTA1 <pierrejoveneljean@gmail.com>
2025-07-19 00:25:48 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "JESUS~CRASH~V1~rkpVhTDb#dcdjwWRH370Eixhebip3jMNTepoMMLrmwqKnO-JQNME",
+SESSION_ID: process.env.SESSION_ID || "JESUS~CRASH~V1~m0oyVILL#Z28X0s5-iRoXszzBGOpoHP4Rt6xaZsV1bY_qH_FYhZg",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -29,7 +29,7 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "JESUS-CRASH-V1",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "JESUS-CRASH-V1",
+STICKER_NAME: process.env.STICKER_NAME || "𝑹𝑬𝒀 ✞ 𝐅𝐓🤵🏾❤️🤴🏾",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,9 +37,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "13058962443",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "8493740033",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DAWENS BOY",
+OWNER_NAME: process.env.OWNER_NAME || "𝑹𝑬𝒀 ✞ 𝐅𝐓🤵🏾❤️🤴🏾",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ by dawens boy*",
 // add bot owner name    
@@ -65,13 +65,13 @@ AUTO_REPLY: process.env.AUTO_REPLY || "true",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "50942241547",
+DEV: process.env.DEV || "8493740033",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
